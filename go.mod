bf5da480b9574e32725e0b59dbc61bb43aa26e90
Cael Rowley <drdeus@pm.me>
2024-02-14 18:46:20 +0100

    feat: use trello api to get all cards and attachments on a board

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gitlab.com/CaelRowley/trello-file-checker
 
 go 1.21.6
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello")
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(".env is missing! " + err.Error())
+	}
+	apiKey, ok := os.LookupEnv("API_KEY")
+	if !ok {
+		panic("API_KEY is missing!")
+	}
+	token, ok := os.LookupEnv("TOKEN")
+	if !ok {
+		panic("TOKEN is missing!")
+	}
+	boardId, ok := os.LookupEnv("BOARD_ID")
+	if !ok {
+		panic("BOARD_ID is missing!")
+	}
+
+	requestUrl := fmt.Sprintf("https://api.trello.com/1/boards/%s/cards?key=%s&token=%s&fields=all&attachments=true", boardId, apiKey, token)
+	response, err := http.Get(requestUrl)
+	if err != nil {
+		panic("Error: " + err.Error())
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic("Error reading response body: " + err.Error())
+	}
+
+	fmt.Println("Response Status:", response.Status)
+	fmt.Println("Response Body:", string(body))
 }
